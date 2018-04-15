b93b74d16c61967580c0b106067707ba57916bbf
Christopher Night <cosmologicon@gmail.com>
2018-04-14 21:06:11 -0400

    PyWeek 25: basic game loop

--- pyweek25/src/main.py
+++ pyweek25/src/main.py
@@ -1,4 +1,28 @@
-from . import settings
+import pygame
+from pygame.locals import *
+from . import settings, view, pview, ptext
 
-print("Welcome to game %s" % settings.gamename)
-print("Press the left mouse button to fire the ducks.")
+pygame.init()
+pview.set_mode(settings.size0, settings.height,
+	forceres = settings.forceres, fullscreen = settings.fullscreen)
+pygame.display.set_caption(settings.gamename)
+clock = pygame.time.Clock()
+playing = True
+while playing:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			playing = False
+		if event.type == KEYDOWN and event.key == K_ESCAPE:
+			playing = False
+		if event.type == KEYDOWN and event.key == K_F10:
+			pview.cycle_height(settings.heights)
+		if event.type == KEYDOWN and event.key == K_F11:
+			pview.toggle_fullscreen()
+		if event.type == KEYDOWN and event.key == K_F12:
+			pview.screenshot()
+	pview.fill((0, 0, 20))
+	ptext.draw(settings.gamename, center = pview.center, color = "white", shade = 2,
+		scolor = "black", shadow = (1, 1), angle = 10,
+		fontsize = pview.T(120))
+	pygame.display.flip()
+
--- pyweek25/src/settings.py
+++ pyweek25/src/settings.py
@@ -1,2 +1,11 @@
 gamename = "Universe Factory 25"
+minfps, maxfps = 10, 60
+
+# Screen resolution
+size0 = 1280, 720
+heights = 360, 480, 576, 720, 1080
+height = 720
+forceres = False
+fullscreen = False
+
 
